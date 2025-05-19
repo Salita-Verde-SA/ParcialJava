cb5d573e17d02cf13e0b3295b51c197464f301e3
FabrizioCastillo <Fabricastillo6@gmail.com>
2025-05-19 12:44:35 -0300

    Añadido del Json Arreglado

--- src/main/java/com/salitaverde/parcial2/persistencia/Persistencia.java
+++ src/main/java/com/salitaverde/parcial2/persistencia/Persistencia.java
@@ -7,11 +7,17 @@ package com.salitaverde.parcial2.persistencia;
 import com.salitaverde.parcial2.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import java.io.File;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 /**
  *
@@ -23,17 +29,38 @@ public class Persistencia {
 
     public static final String ubicacion = System.getProperty("user.dir") + barra + "Registros" + barra;
 
-    public static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-    public static void guardarJson(Autor autor) throws IOException {
-        try (FileWriter escritor = new FileWriter(ubicacion)) {
-                gson.toJson(autor, escritor);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+    public Persistencia() {
     }
 
-}
+    public static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+ public static void guardarJson(Autor nuevoAutor) {
+    List<Autor> autores = new ArrayList<>();
+    File archivo = new File(ubicacion);
+    if (archivo.exists()) {
+        try (Reader lector = new FileReader(archivo)) {
+            // Define el tipo de dato que se va a leer: una lista de autores
+            Type listType = new TypeToken<List<Autor>>() {}.getType();
+            // Convierte el contenido del archivo JSON a una lista de autores
+            autores = gson.fromJson(lector, listType);
+            if (autores == null) {
+                autores = new ArrayList<>();
+            }
+        } catch (JsonSyntaxException e) {
+            System.out.println("El archivo no contenia una lista válida. Se inicializa una nueva.");
+            autores = new ArrayList<>();
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
+
+    autores.add(nuevoAutor);
+
+    try (Writer escritor = new FileWriter(archivo)) {
+         // Convierte la lista de autores a JSON y la guarda en el archivo
+        gson.toJson(autores, escritor);
+    } catch (IOException e) {
+        e.printStackTrace();
+    
+}}}
